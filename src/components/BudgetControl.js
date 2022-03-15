ca1da52c0de982830427ae31cb250cc80a6f3175
Daniel Barbieri <90072728+DHB1985@users.noreply.github.com>
2022-03-15 15:42:18 -0300

    Animation porcentage

--- src/components/BudgetControl.js
+++ src/components/BudgetControl.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {Text, View, Image} from 'react-native';
+import CircularProgress from 'react-native-circular-progress-indicator';
 
 import {amountFormat} from '../utils/utils';
 
@@ -8,24 +9,43 @@ import styles from '../styles/BudgetControlStyles';
 const BudgetControl = ({budget, expenses}) => {
   const [available, setAvailable] = useState(0);
   const [spent, setSpent] = useState(0);
+  const [porcentage, setPorcentage] = useState(0);
 
   useEffect(() => {
-
     const totalExpenses = expenses.reduce(
       (total, spent) => Number(spent.amount) + total,
       0,
     );
-    const totalAvailable = budget - totalExpenses
+    const totalAvailable = budget - totalExpenses;
 
-    setSpent(totalExpenses)
-    setAvailable(totalAvailable)
+    const newPorcentage = (totalExpenses * 100) / budget;
+
+    setTimeout(() => {
+      setPorcentage(newPorcentage);
+    }, 1000);
+    
+    setSpent(totalExpenses);
+    setAvailable(totalAvailable);
   }, [expenses]);
 
   return (
-    
-    <View style={styles.container} >
+    <View style={styles.container}>
       <View style={styles.grafCenter}>
-        <Image style={styles.img} source={require('../img/grafico.jpg')} />
+        {/* <Image style={styles.img} source={require('../img/grafico.jpg')} /> */}
+        <CircularProgress
+          value={porcentage}
+          duration={1500}
+          radius={120}
+          valueSuffix={'%'}
+          // valuePrefix={'$'}
+          title="Gastado"
+          inActiveStrokeColor="#F5F5F5"
+          inActiveStrokeWidth={20}
+          activeStrokeColor="#3B82F6"
+          activeStrokeWidth={20}
+          titleStyle={{fontWeight: 'bold', fontSize: 20}}
+          titleColor="#64748B"
+        />
       </View>
 
       <View style={styles.textContainer}>
